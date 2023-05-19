8aafe792915e33d2ddac5506483eea9d04015113
Maarij <maarij.anwar@gmail.com>
2023-05-19 11:28:17 -0500

    routing slugs

--- routing-events-project/pages/events/[...slug].js
+++ routing-events-project/pages/events/[...slug].js
@@ -1,8 +1,44 @@
+import {useRouter} from "next/router";
+import {getFilteredEvents} from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import {Fragment} from "react";
+
 function FilteredEventsPage() {
+    const router = useRouter();
+
+    const filteredData = router.query.slug;
+
+    if (!filteredData) {
+        return <p className={'center'}>Loading...</p>
+    }
+
+    const filteredYear = filteredData[0];
+    const filteredMonth = filteredData[1];
+
+    const numYear = +filteredYear;
+    const numMonth = +filteredMonth;
+
+    if (isNaN(numYear) || isNaN(numMonth)) {
+        return <p>Invalid filter. Adjust values.</p>
+    }
+
+    const filteredEvents = getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    });
+
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return <p>No events found for the filter!</p>
+    }
+
+    const date = new Date(numYear, numMonth - 1);
+
     return (
-        <div>
-            <h1>Filtered Events</h1>
-        </div>
+        <Fragment>
+            <ResultsTitle date={date}/>
+            <EventList items={filteredEvents}/>
+        </Fragment>
     );
 }
 
