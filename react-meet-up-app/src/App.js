a378f6a65d3fe7bc6626f86f7363e43074f066cb
Maarij <maarij.anwar@gmail.com>
2023-06-13 21:53:38 -0500

    Links & Navigation

--- react-meet-up-app/src/App.js
+++ react-meet-up-app/src/App.js
@@ -2,10 +2,13 @@ import {Route, Routes} from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetUp";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
+
       <Routes>
         <Route path={'/'} element={<AllMeetupsPage/>}/>
         <Route path={'/favorites'} element={<FavoritesPage/>}/>
