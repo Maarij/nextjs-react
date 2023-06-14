75c4f93a1757d0365c4700351bfe25a50260b01c
Maarij <maarij.anwar@gmail.com>
2023-06-13 21:49:35 -0500

    Meet up apps - routing

---
+++ react-meet-up-app/src/App.js
@@ -0,0 +1,18 @@
+import {Route, Routes} from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetUp";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path={'/'} element={<AllMeetupsPage/>}/>
+        <Route path={'/favorites'} element={<FavoritesPage/>}/>
+        <Route path={'/new-meetup'} element={<NewMeetupPage/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
