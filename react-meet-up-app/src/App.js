ea863be183081ee9113543992621a445f8f2b7ca
Maarij <maarij.anwar@gmail.com>
2023-06-30 21:54:54 -0500

    Creating 'wrapper' components

--- react-meet-up-app/src/App.js
+++ react-meet-up-app/src/App.js
@@ -2,18 +2,18 @@ import {Route, Routes} from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetUp";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-
-      <Routes>
-        <Route path={'/'} element={<AllMeetupsPage/>}/>
-        <Route path={'/favorites'} element={<FavoritesPage/>}/>
-        <Route path={'/new-meetup'} element={<NewMeetupPage/>}/>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path={'/'} element={<AllMeetupsPage/>}/>
+          <Route path={'/favorites'} element={<FavoritesPage/>}/>
+          <Route path={'/new-meetup'} element={<NewMeetupPage/>}/>
+        </Routes>
+      </Layout>
     </div>
   );
 }
