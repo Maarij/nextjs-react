b56b8f91cfc21bbe75a88df5b3038da1cdea9087
Maarij <maarij.anwar@gmail.com>
2023-07-13 17:04:31 -0500

    rending dummy post data

--- full-blog/components/posts/post-item.js
+++ full-blog/components/posts/post-item.js
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import Link from 'next/link';
+import Image from 'next/image';
 import classes from './post-item.module.css';
 
 function PostItem(props) {
@@ -11,11 +12,12 @@ function PostItem(props) {
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
 
   return <li className={classes.post}>
-    <Link href={''}>
+    <Link href={linkPath}>
       <div className={classes.image}>
-        <Image src={imagePath} alt={title} width={300} height={200} />
+        <Image src={imagePath} alt={title} width={300} height={200} layout={"responsive"}/>
       </div>
       <div className={classes.content}>
         <h3>{title}</h3>
