cea4fd93bd95f89ad77eadf6308707dd3746d0c3
Maarij <maarij.anwar@gmail.com>
2023-07-15 15:05:09 -0500

    preparing the contact form

--- full-blog/pages/contact.js
+++ full-blog/pages/contact.js
@@ -1,5 +1,7 @@
-function Contact() {
+import ContactForm from "../components/contact/contact-form";
 
+function ContactPage() {
+  return <ContactForm />
 }
 
-export default Contact;
+export default ContactPage;
